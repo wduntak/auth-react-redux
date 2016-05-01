ab4e990f1ad6248e81f48708f77125930c97a2b4
wduntak <wduntak@gmail.com>
2016-04-30 21:13:09 -0400

    Add nodemon to autorefresh changes

--- server/index.js
+++ server/index.js
@@ -3,15 +3,19 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+
 const app = express();
 
 // App Setup
-// Morgan is a logging framework
-app.use(morgan('combined'));
-app.use(bodyParser.json({ type: '*/*' }));
+
+	// Morgan is a logging framework
+	app.use(morgan('combined'));
+	// Parse incoming requests into JSON
+	app.use(bodyParser.json({ type: '*/*' }));
 
 // Server Setup
-const port = process.env.PORT || 3030;
-const server = http.createServer(app);
-server.listen(port);
-console.log('Server listening on:', port);
+
+	const port = process.env.PORT || 3030;
+	const server = http.createServer(app);
+	server.listen(port);
+	console.log('Server listening on:', port);
